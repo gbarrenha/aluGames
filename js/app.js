31a4ecc3311d723e034d0f6b8891857c3b793902
Gabriel Barrenha <barrenha.gabriel@gmail.com>
2026-01-15 10:41:18 -0300

    list update in JS and HTML

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+let jogos = [
+  { id: 1, nome: `Monopoly`, imagem: `monopoly.png`, alugado: false },
+  { id: 2, nome: `Ticket to Ride`, imagem: `ticket_to_ride.png`, alugado: false },
+  { id: 3, nome: `Takenoko`, imagem: `takenoko.png`, alugado: false },
+];
+
+function renderizarJogos() {
+  let lista = document.getElementById(`lista-jogos`);
+  lista.innerHTML = ``;
+
+  jogos.forEach((jogo) => {
+    lista.innerHTML += `
+      <li class="dashboard__items__item" id="game-${jogo.id}">
+        <div class="dashboard__item__img ${jogo.alugado ? `dashboard__item__img--rented` : ``}">
+        <img src="img/${jogo.imagem}" alt="${jogo.nome}">
+        </div>
+        <p class="dashboard__item__name">${jogo.nome}</p>
+        <a href="#" class="dashboard__item__button ${jogo.alugado ? `dashboard__item__button--return` : ``}" onclick="alterarStatus(${jogo.id})">
+          ${jogo.alugado ? `Devolver` : `Alugar`}
+        </a>
+      </li>
+      `;
+  });
+}
+
+renderizarJogos();
+
 function alterarStatus(id) {
   let gameClicado = document.getElementById(`game-${id}`);
   let imagem = gameClicado.querySelector(`.dashboard__item__img`);
